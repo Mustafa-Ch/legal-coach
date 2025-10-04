8dd5f925934317d07fc151e17a6beacbc6525e91
Rashid <rashid.khan@qfnetwork.org>
2025-10-04 22:00:41 +0500

    ...

--- legal-coach/strapi-cms/src/admin/vite.config.example.ts
+++ legal-coach/strapi-cms/src/admin/vite.config.example.ts
@@ -1,11 +1,13 @@
-import { mergeConfig, type UserConfig } from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { mergeConfig, type UserConfig } from "vite";
 
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
       alias: {
-        '@': '/src',
+        "@": "/src",
       },
     },
   });
